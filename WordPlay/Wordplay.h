7c52d63bfae6a393eb346eb5807504131f9092f6
diegocichello <diegocichello@Diegos-MacBook-Pro.local>
2015-01-07 10:26:58 -0800

    Mad Libs

---
+++ WordPlay/Wordplay.h
@@ -0,0 +1,13 @@
+//
+//  Wordplay.h
+//  WordPlay
+//
+//  Created by Diego Cichello on 1/7/15.
+//  Copyright (c) 2015 Kevin Lee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Wordplay : NSObject
+
+@end
